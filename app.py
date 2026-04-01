2aa4f621c255223ad2baaa1269e036840dd9eeee
Arju Parween <arjuparween20308@gmail.com>
2026-04-01 16:34:48 +0530

    final project upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+# download (Render ke liye important)
+nltk.download('punkt')
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+
+def transform_text(text):
+    text = text.lower()
+    words = nltk.word_tokenize(text)
+
+    words = [w for w in words if w.isalnum()]
+    words = [w for w in words if w not in stop_words]
+    words = [ps.stem(w) for w in words]
+
+    return " ".join(words)
+
+# load model
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_sms = request.form['message']
+
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        prediction = "Spam"
+    else:
+        prediction = "Not Spam"
+
+    return render_template('index.html', prediction=prediction, text=input_sms)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
